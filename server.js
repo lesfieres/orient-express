db06844b6ee6c6d58c2d01f72dc874d07f1ce086
MarcPuigdemont <marcpuigdemontriera@gmail.com>
2019-03-18 13:49:18 +0100

    Added GoodreadsService from api-services

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const ApiServices = require('api-services');
+const env = require('dotenv');
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+let config = env.config().parsed;
+const goodreadsService = new ApiServices.GoodreadsService(
+  config.GOODREADS_KEY,
+  config.GOODREADS_SECRET
+);
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.get('/search', function(req, res) {
+  const title = req.query.title;
+  const fromPage = req.query.from;
+  const toPage = req.query.to;
+  console.log(title, fromPage, toPage)
+  goodreadsService.search(title, fromPage, toPage).then((books) => {
+    console.log(books.length);
+    res.send(books);
+  });
+});
+
+const server = app.listen(8081, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
