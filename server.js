1a0e529f536b35602f362d15471cccdc5ab64249
Marc Verdaguer Ginestera <marc.verdaguer@getinge.com>
2019-03-29 08:39:27 +0100

    Adding localhost as ip.

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/get-movie', function(req, res) {
   });
 });
 
-const server = app.listen(8081, function() {
+const server = app.listen(8081, "localhost", function() {
   const host = server.address().address;
   const port = server.address().port;
 
